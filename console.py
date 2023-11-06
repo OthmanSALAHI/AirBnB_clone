f0424ea2824c8a225e33c3cc01b7a86a2916bc1c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-06 19:47:53 +0100

    console 0.1.0

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """defines the HBNBCommand class"""
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """represents the entry point of the command interpreter"""
@@ -10,11 +12,26 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
-    do_EOF = do_quit
+    
+    def do_EOF(self, argu):
+        """EOF command to exit the program"""
+        print("")
+        return True
 
     def emptyline(self):
         """Empty line shouldn't execute anything"""
         pass
 
+    def do_create(self, arg):
+        """Create a new instance of BaseModel and save it"""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
+            print("** class doesn't exist **")
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
