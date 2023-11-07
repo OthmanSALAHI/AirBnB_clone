b662b9640a00cfe4fa91c0a48190d412f45362c8
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-07 12:40:24 +0100

    add test and pycodestyle

--- models/state.py
+++ models/state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
- 
+
 class State(BaseModel):
-    """ the inhreit class of BaseModel 
+    """ the inhreit class of BaseModel
     Attributes:
         name (str): the name of state
     """
