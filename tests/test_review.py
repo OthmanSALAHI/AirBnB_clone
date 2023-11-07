5a246f4c3c5393f8e2c89ef308c90eaf6ecf1f19
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-07 20:12:26 +0100

    test the review

--- tests/test_review.py
+++ tests/test_review.py
@@ -4,27 +4,29 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime as dt
 import pycodestyle
-from models.city import City
+from models.review import Review
 import models
 from io import StringIO
 import sys
 from unittest.mock import patch
 
 
-class Testcity(unittest.TestCase):
+class Testreview(unittest.TestCase):
     """test the state class"""
-    def test_thecity(self):
-        """test the City"""
-        new = City()
+    def test_thereview(self):
+        """test the review"""
+        new = Review()
         self.assertTrue(hash(new), "id")
-        self.assertTrue(hash(new), "state_id")
+        self.assertTrue(hash(new), "place_id")
+        self.assertTrue(hash(new), "user_id")
         self.assertTrue(hasattr(new, "created_at"))
         self.assertTrue(hasattr(new, "updated_at"))
-        self.assertTrue(hasattr(new, "name"))
+        self.assertTrue(hasattr(new, "text"))
         """type test"""
-        self.assertIsInstance(new.name, str)
         self.assertIsInstance(new.id, str)
-        self.assertIsInstance(new.state_id, str)
+        self.assertIsInstance(new.place_id, str)
+        self.assertIsInstance(new.user_id, str)
+        self.assertIsInstance(new.text, str)
         self.assertIsInstance(new.created_at, dt)
         self.assertIsInstance(new.updated_at, dt)
 
@@ -33,7 +35,7 @@ class Testpycodestyle(unittest.TestCase):
     def test_pycode(self):
         """ test pycodestyle """
         pycode = pycodestyle.StyleGuide(quiet=True)
-        result = pycode.check_files(["models/city.py"])
+        result = pycode.check_files(["models/review.py"])
         MessageErr = "code style error pr warning !"
         self.assertEqual(result.total_errors, 0, MessageErr)
 
