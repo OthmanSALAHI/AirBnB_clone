1fe109846912b4e84be89d7e91f43df63a00e80c
OthmanSALAHI <ootmansalhi.24@gmail.com>
2023-11-07 20:08:34 +0100

    test city and modifie it

--- models/city.py
+++ models/city.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
 
- 
+
 class City(BaseModel):
-    """ the inhreit class of BaseModel 
+    """ the inhreit class of BaseModel
     Attributes:
         state_id (str): the id of state
         name (str): the name of city
     """
     state_id = ""
     name = ""
-        
