d31aaa75a520e97ae2ac674d49db879f501dadfb
danimohamed <mohameddani993@gmail.com>
2023-11-08 10:28:54 +0100

    pycodestyle is done ✔️

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ class City(BaseModel):
     """ the inhreit class of BaseModel
     Attributes:
         state_id (str): the id of state
-        name (str): the name of city
+        name (str ): the name of city
     """
     state_id = ""
     name = ""
